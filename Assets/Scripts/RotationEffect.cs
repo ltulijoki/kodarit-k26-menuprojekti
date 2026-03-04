dcec0644d31a5baf537537ae98866a8bab93cd54
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2026-03-04 19:31:35 +0200

    lisätty tekstien pyöriminen

---
+++ Assets/Scripts/RotationEffect.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class RotationEffect : MonoBehaviour
+{
+    public float angle = 10f;
+    public float speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tilt = Mathf.Sin(Time.time * speed) * angle;
+        transform.localRotation = Quaternion.Euler(0, 0, tilt);
+    }
+}
