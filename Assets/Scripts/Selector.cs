31835c3e9f689ce534027a17db79cb6702b24f5d
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2026-03-18 19:31:01 +0200

    lisätty värit selectoriin

--- Assets/Scripts/Selector.cs
+++ Assets/Scripts/Selector.cs
@@ -4,12 +4,14 @@ using TMPro;
 public class Selector : MonoBehaviour
 {
     public TMP_Text[] items;
+    public Color normalColor = Color.white;
+    public Color highlightColor = Color.yellow;
     private int index = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UpdateColors();
     }
 
     // Update is called once per frame
@@ -18,16 +20,31 @@ public class Selector : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             index = (index - 1 + items.Length) % items.Length;
+            UpdateColors();
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             index = (index + 1) % items.Length;
+            UpdateColors();
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Index on " + index);
+            ActivateItem(index);
         }
+    }
+
+    void UpdateColors()
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i].color = (i == index) ? highlightColor : normalColor;
+        }
+    }
+
+    public void ActivateItem(int i)
+    {
+        Debug.Log("Selected: " + items[i].text);
     }
 }
