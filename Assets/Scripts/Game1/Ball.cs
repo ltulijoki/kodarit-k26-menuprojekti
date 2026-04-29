6b5b9a6a2a2a1f00da1b5f02e1ec6e3758de34ce
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2026-04-29 19:31:03 +0300

    lisätty pelaajan liikkuminen

---
+++ Assets/Scripts/Game1/Ball.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Launch();
+    }
+
+    public void Launch()
+    {
+        rb.linearVelocity = Vector2.up * 100;
+    }
+}
